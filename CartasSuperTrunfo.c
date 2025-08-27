9b55dc545fa168e5737aca7c765c7498f0327cff
CaduPedrosa <carlosProriz17@outlook.com>
2025-08-27 19:33:45 +0000

    Nivel Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,11 +9,11 @@
 int main() {
     char estado1, codigocarta1[50],nomeCidade1[50];
     int populacao1, pontosturistico1;
-    float area1, pib1;
+    float area1, pib1, densidadepop1, pibpercapita1;
 
     char estado2, codigocarta2[50],nomeCidade2[50];
     int populacao2, pontosturistico2;
-    float area2, pib2;
+    float area2, pib2, densidadepop2, pibpercapita2;
 
     printf("Digite o estado da primeira carta: \n");
     scanf(" %c", &estado1);
@@ -58,10 +58,18 @@ int main() {
     printf("Digite o número de pontos turísticos: \n");
     scanf("%d", &pontosturistico2);
 
-    printf("\nCarta 1:\n Estado: %c\n Código: %s\n Nome da cidade: %s\n População: %d\n Área: %.2f Km\n PIB: %f\n Números de pontos turísticos: %d\n", estado1, codigocarta1, nomeCidade1, populacao1, area1, pib1, pontosturistico1);
+    // Calculo Nivel Aventureiro CARTA 1 E 2
+    densidadepop1 = populacao1 / area1;
+    pibpercapita1 = pib1 / populacao1;
+
+    densidadepop2 = populacao2 / area2;
+    pibpercapita2 = pib2 / populacao2;
+
+    //carta 1
+    printf("\nCarta 1:\n Estado: %c\n Código: %s\n Nome da cidade: %s\n População: %d\n Área: %.2f Km\n PIB: %f\n Números de pontos turísticos: %d\n Densidade Populacional: %.2f\n PIB per capita: %.2f", estado1, codigocarta1, nomeCidade1, populacao1, area1, pib1, pontosturistico1, densidadepop1,pibpercapita1);
 
     //carta 2
-     printf("\nCarta 2:\n Estado: %c\n Código: %s\n Nome da cidade: %s\n População: %d\n Área: %.2f Km\n PIB: %f\n Números de pontos turísticos: %d\n", estado2, codigocarta2, nomeCidade2, populacao2, area2, pib2, pontosturistico2);
+     printf("\nCarta 2:\n Estado: %c\n Código: %s\n Nome da cidade: %s\n População: %d\n Área: %.2f Km\n PIB: %f\n Números de pontos turísticos: %d\n Densidade Populacional: %.2f\n PIB per capita: %.2f", estado2, codigocarta2, nomeCidade2, populacao2, area2, pib2, pontosturistico2, densidadepop2, pibpercapita2);
 
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
